d3bdd36e7e3ab81212f2c7429f35c967594ef91d
Sasuke27K <kelvintsga@outlook.com>
2017-07-19 11:14:32 -0500

    agregando contenido de imprimirtesis

--- src/AlgoritmoNuevo.java
+++ src/AlgoritmoNuevo.java
@@ -3,7 +3,32 @@ public class AlgoritmoNuevo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println("hola mundo");
 
+int i = 0;
+int nota=0;
+
+boolean copiaEnExamen = true;
+
+do{
+	
+	if (i>5)
+	{
+		System.out.println("tu nota es " + nota	);
+		
+		break;
+		
+	}
+	i++;
+}while(copiaEnExamen);
 	}
 
+	public static void imprimirTesis(){
+		System.out.println("hola mundo");
+		System.out.println("soy sasuke27k");
+		System.out.println("jejejejeje");
+	}
+	
+	
+	
 }
